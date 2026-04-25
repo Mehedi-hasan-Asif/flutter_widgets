324148ea9798ca04e3b5f4eafc0d895a9f7091da
user <mehedihasanasif2@gmail.com>
2026-04-25 09:43:16 +0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,29 +13,63 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Container"),
+          title: Text("Row & Column"),
           centerTitle: true,
           backgroundColor: Colors.blue,
         ),
-        body: Center(
-          child: Container(
-            height: 200,
-            width: 200,
-            padding: const EdgeInsets.all(20),
-            margin: const EdgeInsets.all(30),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.amber, // এখানে color দিতে হবে
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(color: Colors.black, width: 3),
-            ),
-            child: const Text(
-              "HELLO",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              //Horizontal
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 60,
+                    height: 60,
+                    color: Colors.red,
+                    alignment: Alignment.center,
+                    child: Text("Hi", style: TextStyle(color: Colors.white)),
+                  ),
+                  Container(
+                    width: 60,
+                    height: 60,
+                    color: Colors.green,
+                    alignment: Alignment.center,
+                    child: Text("Naina", style: TextStyle(color: Colors.white)),
+                  ),
+                ],
               ),
-            ),
+              SizedBox(height: 20,),
+              //Vertical
+              Column(
+
+
+                children: [
+                  Container(
+                    width: 100,
+                    height: 50,
+                    color: Colors.orange,
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Hi ",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 50,
+                    color: Colors.purple,
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Rahul",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
