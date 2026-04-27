cf63cd28062e6c7c166d3eed8ad012ddcde336b6
user <mehedihasanasif2@gmail.com>
2026-04-27 11:38:43 +0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,47 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Image"),
-          backgroundColor: Colors.blue,
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Image.asset(
-            "images/app.jpg",
-            width: 200,
-            height: 200,
-          ),
-        ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("ListView"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.builder(
+        itemCount: 30,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(title: Text("$index"));
+        },
+        // body: ListView(
+        //   children: [
+        //     Container(color: Colors.red, height: 100),
+        //     Divider(),
+        //     Container(color: Colors.orange, height: 100),
+        //     Divider(),
+        //     Container(color: Colors.purple, height: 100),
+        //     Divider(),
+        //     Container(color: Colors.amber, height: 100),
+        //     Divider(),
+        //     Container(color: Colors.greenAccent, height: 100),
+        //     Divider(),
+        //     Container(color: Colors.indigo, height: 100),
+        //     Divider(),
+        //     Container(color: Colors.green, height: 100),
+        //     Divider(),
+        //     Container(color: Colors.limeAccent, height: 100),
+        //   ],
       ),
     );
   }
